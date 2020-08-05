e83dba4a36f5e674abddb098633d949d799d3c55
Jonathan Rauprich <rauprich.jonathan@googlemail.com>
2020-08-05 12:45:30 +0200

    Move close button to top left for ios.

--- src/ios/PhotoViewer.m
+++ src/ios/PhotoViewer.m
@@ -229,7 +229,7 @@
         [closeBtn setTitle:@"✕" forState:UIControlStateNormal];
         closeBtn.titleLabel.font = [UIFont systemFontOfSize: 32];
         [closeBtn setTitleColor:[UIColor colorWithRed:255/255.0 green:255/255.0 blue:255/255.0 alpha:0.6] forState:UIControlStateNormal];
-        [closeBtn setFrame:CGRectMake(0, viewHeight - 50, viewWidth, 50)];
+        [closeBtn setFrame:CGRectMake(viewWidth - 50, 0, 50, 50)];
         closeBtn.contentHorizontalAlignment = UIControlContentHorizontalAlignmentLeft;
         closeBtn.contentEdgeInsets = UIEdgeInsetsMake(0, 10, 0, 0);
         [closeBtn setBackgroundColor:[UIColor colorWithRed:0.0 green:0.0 blue:0.0 alpha:0.6]];
@@ -286,7 +286,7 @@
         [fullView setFrame:CGRectMake(0, 0, viewWidth, viewHeight)];
         [imageView setFrame:CGRectMake(0, 0, viewWidth, viewHeight)];
         fullView.contentSize = imageView.frame.size;
-        [closeBtn setFrame:CGRectMake(0, viewHeight - 50, 50, 50)];
+        [closeBtn setFrame:CGRectMake(viewWidth - 50, 0, 50, 50)];
     }
 }
 
